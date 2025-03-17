b9ae00dc71a8202fa0f6a43942b3acfe9f1e3077
DIDER <Simon.dider@gmail.com>
2025-03-17 11:12:18 +0100

    test mappe tilføj

---
+++ Test/controller/ControllerTest.java
@@ -0,0 +1,18 @@
+import controller.Controller;
+import ordination.DagligFast;
+import ordination.DagligSkaev;
+import ordination.Dosis;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class ControllerTest {
+    public Controller controller;
+
+
+}
