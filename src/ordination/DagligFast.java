5cba291d1b340d718be9e11f24d65f699b7874af
Rune <eaa24rhje@students.eaaa.dk>
2025-03-17 10:46:39 +0100

    SamletDosis

--- src/ordination/DagligFast.java
+++ src/ordination/DagligFast.java
@@ -2,18 +2,19 @@ package ordination;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 
-public class DagligFast extends Ordination{
+public class DagligFast extends Ordination {
     private Dosis[] dosis = new Dosis[4];
 
     public DagligFast(LocalDate startDato, LocalDate slutDato) {
         super(startDato, slutDato);
     }
 
-    public Dosis opretDosis(LocalTime tid, double antal){
+    public Dosis opretDosis(LocalTime tid, double antal) {
         Dosis newDosis = new Dosis(tid, antal);
         for (int i = 0; i < dosis.length; i++) {
-            if (dosis[i] == null){
+            if (dosis[i] == null) {
                 dosis[i] = newDosis;
             }
         }
@@ -25,9 +26,11 @@ public class DagligFast extends Ordination{
     public double samletDosis() {
         double result = 0;
 
-        for (Dosis dosi : dosis) {
-            if (dosi != null){
-                result += dosi.getAntal();
+        for (int i = 0; i < (ChronoUnit.DAYS.between(getStartDato(), getSlutDato())); i++) {
+            for (Dosis dosi : dosis) {
+                if (dosi != null) {
+                    result += dosi.getAntal();
+                }
             }
         }
         return result;
